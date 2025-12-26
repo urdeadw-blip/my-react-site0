4d7846e8f1b926018d87ba2a7a3da839523510ca
urdeadw-blip <urdeadw@gmail.com>
2025-12-26 18:36:41 +0100

    Update vite.config.ts

--- me/vite.config.ts
+++ me/vite.config.ts
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
-
 import { miaodaDevPlugin } from "miaoda-sc-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',  // <--- أضف هذا السطر (الأهم!)
   plugins: [react(), svgr({
       svgrOptions: {
-        icon: true, exportType: 'named', namedExport: 'ReactComponent', }, }), miaodaDevPlugin()],
+        icon: true,
+        exportType: 'named',
+        namedExport: 'ReactComponent',
+      },
+    }), miaodaDevPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
